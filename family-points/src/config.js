9d23b6e1dcbdc378ffe7ff2891d97d6b9a48583d
brandonburtner <57689512+brandonburtner@users.noreply.github.com>
2026-04-18 13:04:26 -0600

    Update config.js

--- family-points/src/config.js
+++ family-points/src/config.js
@@ -2,7 +2,7 @@ export const config = {
   cognito: {
     domain: "us-east-2ecrfu9jqz.auth.us-east-2.amazoncognito.com",
     clientId: "41e3ge29dk4g3ebo2p124nu747",
-    redirectUri: "https://brandonburtner.com/family-bank/callback",
+    redirectUri: "https://brandonburtner.com/family-bank/",
     logoutUri: "https://brandonburtner.com/family-bank",
     scopes: "email openid profile"
   },
